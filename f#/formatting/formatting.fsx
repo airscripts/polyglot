b4ef448ad8054b57b6db3cdc6493390b45507836
Airscript <dev.airscript@gmail.com>
2022-12-08 17:44:25 +0100

    feat(f#): add operators script

--- f#/formatting/formatting.fsx
+++ f#/formatting/formatting.fsx
@@ -2,5 +2,4 @@ let animal = "fox"
 let action = "jumps"
 let color = "brown"
 let number = 5
-
 printf $"The {color} {animal} {action} the bridge {number} times."
---
+++ f#/operators/operators.fsx
@@ -0,0 +1,3 @@
+let number = 25
+let isNumberDivisibleByTwo = 25 % 2 = 0
+printf "%b" isNumberDivisibleByTwo
