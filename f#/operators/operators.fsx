21a5d263df834b8c3d2b81b70f9145499ac15dc8
Airscript <dev.airscript@gmail.com>
2022-12-08 17:59:08 +0100

    feat(f#): add more operators inside operators script

--- f#/operators/operators.fsx
+++ f#/operators/operators.fsx
@@ -1,3 +1,23 @@
-let number = 25
-let isNumberDivisibleByTwo = 25 % 2 = 0
-printf "%b" isNumberDivisibleByTwo
+let mutable number = 25
+let isNumberDivisibleByTwo = number % 2 = 0
+printfn "%b" isNumberDivisibleByTwo
+
+number <- 20
+let sum = number + 15
+printfn "%i" sum
+
+number <- 32
+let subtraction = number - 12
+printfn "%i" subtraction
+
+number <- 2
+let division = number / 2
+printfn "%i" division
+
+number <- 42
+let areNumbersEqual = number = 42
+printfn "%b" areNumbersEqual
+
+number <- 128
+let areNumbersUnequal = number <> 1
+printfn "%b" areNumbersUnequal
