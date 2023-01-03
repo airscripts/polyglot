729ad633aa282fca3f4fe6cfe7875bca4b998690
Airscript <francesco@airscript.it>
2023-01-03 18:01:03 +0100

    feat(nim): add slices in uncategorized folder

---
+++ nim/fundamentals/uncategorized/slices.nim
@@ -0,0 +1,7 @@
+var 
+  firstPhrase: string = "Nim is a programming language."
+  secondPhrase: string = "Slices are useless."
+
+echo firstPhrase[0..2]
+secondPhrase[^8..^2] = "useful"
+echo secondPhrase
