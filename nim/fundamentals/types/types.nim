35d41a9d98156f5534149d130fb1dd6eafb9b1c6
Airscript <francesco@airscript.it>
2023-01-03 18:02:13 +0100

    feat(nim): add types in types folder

---
+++ nim/fundamentals/types/types.nim
@@ -0,0 +1,14 @@
+let isGreen: bool = true
+let letter: char = 'F'
+let word: string = "Penguin"
+let x: int = 32
+let f: uint = 34'u
+let a: int8 = 1'i8
+let b: int16 = 2'i16
+let y: int32 = 21'i32
+let z: int64 = 12'i64
+let fx: float = 1.0
+let fy: float32 = 1.0'f32
+let fz: float64 = 2.0'f64
+
+echo isGreen, letter, word, x, f, a, b, y, z, fx, fy, fz
