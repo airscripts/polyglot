1459149045b01caffe5c424ef827411e5f4e8746
Airscript <francesco@airscript.it>
2023-01-03 18:41:50 +0100

    refactor(f#): remove sum script

--- f#/fundamentals/sum.fsx
+++
@@ -1,8 +0,0 @@
-printf "Type the first number: "
-let firstNumber = System.Console.ReadLine()
-
-printf "Type the second number: "
-let secondNumber = System.Console.ReadLine()
-
-let sum = (int firstNumber) + (int secondNumber)
-printfn $"The sum is: {sum}"
