3451ca479a18ee3e3d13545748a59da573a1d7d4
Airscript <francesco@airscript.it>
2023-01-06 04:30:43 +0100

    feat(nim): add dynamic status, version and date to status api in http-server poc

--- nim/http-server/api.nim
+++ nim/http-server/api.nim
@@ -1,6 +1,8 @@
+import std/json
 import std/asyncdispatch
 
 from std/strformat import fmt
+from std/times import now, utc, DateTime, format
 from std/asynchttpserver import Request, respond, Http404, Http200
 
 proc root*(request: Request) {.async.} =
@@ -12,8 +14,11 @@ proc ping*(request: Request) {.async.} =
   await request.respond(code=Http200, content=content)
 
 proc status*(request: Request) {.async.} =
-  let content: string = fmt"status: {true}"
-  await request.respond(code=Http200, content=content)
+  let status: bool = true
+  let version: string = "1.0"
+  let date = now().format("yyyy-MM-dd '@' hh:mm:ss")
+  let content = %* {"status": status, "version": version, "date": date}
+  await request.respond(code=Http200, content=fmt"{content}")
 
 proc poweroff*(request: Request) {.async.} =
   let content: string = fmt"shutdown: {true}"
