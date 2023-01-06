aa36fc30d8e3b88baacb18d3b23efe7f0c8fa2f8
Airscript <francesco@airscript.it>
2023-01-06 03:47:34 +0100

    feat(nim): add api module template for http-client poc

--- nim/http-server/router.nim
+++ nim/http-server/router.nim
@@ -1,11 +1,13 @@
 import std/asyncdispatch 
 import std/asynchttpserver
 
+from api as api import root, ping, hello, error
+
 proc router*(req: Request) {.async.} =
   let path = req.url.path
 
   case path:
-    of "/": discard
-    of "/ping": discard
-    of "/hello": discard
-    else: discard
+    of "/": root()
+    of "/ping": ping()
+    of "/hello": hello()
+    else: error()
