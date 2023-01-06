e7e2017f47db4aed99cf37eb0843f60cc127fa42
Airscript <francesco@airscript.it>
2023-01-06 16:56:26 +0100

    refactor(nim): add main module before function call in tests file in unittest poc

--- nim/unittest/tests.nim
+++ nim/unittest/tests.nim
@@ -10,22 +10,22 @@ suite "Calculator functions test suite":
     discard
 
   test "Add function":
-    check(add(15, 18) == 33)
-    check(add(7, 35) != 43)
-    check(add(12.33, 4.11) == 16.44)
-    check(add(1.74, 1.73) != 1)
+    check(main.add(15, 18) == 33)
+    check(main.add(7, 35) != 43)
+    check(main.add(12.33, 4.11) == 16.44)
+    check(main.add(1.74, 1.73) != 1)
 
   test "Sub function":
-    check(sub(1, 5) == -4)
-    check(sub(15, 5) != 11)
-    check(sub(33.1, 13.1) == 20)
-    check(sub(11.1, 10.1) != 1.1)
+    check(main.sub(1, 5) == -4)
+    check(main.sub(15, 5) != 11)
+    check(main.sub(33.1, 13.1) == 20)
+    check(main.sub(11.1, 10.1) != 1.1)
 
   test "Mul function":
-    check(mul(5, 5) == 25)
-    check(mul(10, 1) != 11)
-    check(mul(11.1, 2.0) == 22.2)
-    check(mul(5.5, 3.0) != 12)
+    check(main.mul(5, 5) == 25)
+    check(main.mul(10, 1) != 11)
+    check(main.mul(11.1, 2.0) == 22.2)
+    check(main.mul(5.5, 3.0) != 12)
 
   test "Div function":
     check(main.div(12, 2) == 6)
