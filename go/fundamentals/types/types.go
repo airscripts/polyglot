ee01a3ef31d4a3504ec4b8e1ba6f23685442f0cb
Airscript <francesco@airscript.it>
2023-08-09 01:33:23 +0200

    feat(go): add types in fundamentals

---
+++ go/fundamentals/types/types.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a bool = true
+	var b int32 = 12
+	var c float64 = 5.12
+	var d string = "Airscript"
+	var e rune = 123
+	var f byte = 255
+	var g complex128 = complex(1, 2)
+	var h uint8 = 1
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println(g)
+	fmt.Println(h)
+}
